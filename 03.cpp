7add0c68d778659a2dbf7e40fed2ceb645c0eef7
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-01-29 15:28:46 +0530

    First commit

---
+++ 03.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.length() != t.length()){
+            return false;
+       } 
+
+       int arr[26] = {0};
+
+       for(char i : s){
+        arr[i - 'a']++;
+       }
+
+       for(char i : t){
+        arr[i - 'a']--;
+        if(arr[i - 'a'] < 0) return false; 
+       }
+       return true;
+    }
+};
