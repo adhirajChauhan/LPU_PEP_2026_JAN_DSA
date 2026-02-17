782f302c2efb39cf7c7f07ec3547e7b1e75ad7dd
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-02-17 10:23:09 +0530

    17022026

--- DSA/07_Trees/07.cpp
+++ DSA/07_Trees/07.cpp
@@ -195,3 +195,66 @@ public:
         return result;
     }
 };
+
+
+
+
+https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        vector<int> result;
+        
+        if(root==NULL) return result;
+        
+        //we will create a map to store first node at each horizontal distance
+        // key -> horizontal distance - hd
+        // value -> node value
+        map<int,int> topNode;
+        
+        //create a queue to store pair of node and hd
+        queue<pair<Node*, int>> q;
+        
+        //start with root at hd-> 0
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()){
+            //extract the front pair
+            pair<Node*, int> frontVal =  q.front();
+            q.pop();
+            
+            Node* node = frontVal.first;
+            int hd = frontVal.second;
+            
+            if(topNode.find(hd) == topNode.end()){
+                topNode[hd] = node->data;
+            }
+            if(node->left) q.push(make_pair(node->left, hd-1));
+            if(node->right) q.push(make_pair(node->right, hd+1));
+            
+        }
+        
+        for(auto it : topNode){
+            result.push_back(it.second);
+        }
+        
+        
+    }
+};
