7eb8a4980c1a868bfe3f55af0ec83fe0c4dbebef
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-02-18 10:30:51 +0530

    18022026

--- DSA/07_Trees/07.cpp
+++ DSA/07_Trees/07.cpp
@@ -258,3 +258,90 @@ class Solution {
         
     }
 };
+
+
+
+https://www.geeksforgeeks.org/problems/k-distance-from-root/1
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+class Solution {
+  public:
+    // function should print the nodes at k distance from root
+    vector<int> Kdistance(Node *root, int k) {
+        // Your code here
+        // if root is null -> return empty list
+        // init a queue and push root
+        // track current distance = 0
+        // loop while queue is not empty:
+        //     if current distance == k -> collect all nodes in queuue and return
+        //     else process current level:
+        //         traverse current level nodes
+        //         push children to queue
+        //     increment distance
+        
+        vector<int> result;
+        if(root == NULL) return result;
+        queue<Node*> q;
+        q.push(root);
+        int currentDistance = 0;
+        
+        while(!q.empty()){
+            int size = q.size();
+            if(currentDistance == k){
+                while(!q.empty()){
+                    result.push_back(q.front()->data);
+                    q.pop();
+                }
+                break;
+            }
+            while(size--){
+                Node* node = q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            currentDistance++;
+        }
+        return result;
+    }
+};
+
+
+
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+
+        if(root == p || root == q) return root;
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p, q);
+
+        if(leftLCA && rightLCA){
+            return root;
+        }
+        else if(leftLCA != NULL) return leftLCA;
+        else return rightLCA;
+
+
+    }
+};
