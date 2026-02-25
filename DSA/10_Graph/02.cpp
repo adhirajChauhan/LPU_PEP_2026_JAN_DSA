14a246dcabef4adfeba36d599eb674800816cb46
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-02-25 09:55:09 +0530

    25022026

---
+++ DSA/10_Graph/02.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <list>
+using namespace std;
+ 
+int main()
+{
+    int n; //number of vertices
+    int m; // number of edges
+
+    cout << "Enter the number of vertices : ";
+    cin >> n;
+    cout << "Enter the number of edges : ";
+    cin >> m;
+
+    unordered_map<int, list<int>> adjList;
+    cout << "Enter edges (u , v) : " << endl;
+    // cout << "\nAdjacency List : " << endl;
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    cout << "Adjacency List : " << endl;
+    for(auto i : adjList){
+        cout << i.first << " -> ";
+
+        for(auto j : i.second){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    
+}
