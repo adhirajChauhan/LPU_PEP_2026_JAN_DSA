fd5ca5de0e65bd7e6da31ec08454ed8dee6f7380
Adhiraj <adhirajsingh0105@gmail.com>
2026-07-01 22:53:33 +0530

    Update 03.cpp

--- DSA/02_Strings/03.cpp
+++ DSA/02_Strings/03.cpp
@@ -88,3 +88,42 @@ class Solution {
         
     }
 };
+
+
+https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/
+
+class Solution {
+public:
+
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' ||
+               ch == 'o' || ch == 'u';
+    }
+
+    int maxVowels(string s, int k) {
+
+        int count = 0;
+
+        // First window
+        for(int i = 0; i < k; i++) {
+            if(isVowel(s[i]))
+                count++;
+        }
+
+        int ans = count;
+
+        // Sliding window
+        for(int i = k; i < s.size(); i++) {
+
+            if(isVowel(s[i - k]))
+                count--;
+
+            if(isVowel(s[i]))
+                count++;
+
+            ans = max(ans, count);
+        }
+
+        return ans;
+    }
+};
