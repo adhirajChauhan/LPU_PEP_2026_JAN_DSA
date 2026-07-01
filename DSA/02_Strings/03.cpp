174663f71f01f1222d16f641af1a1e5b2b117b27
Adhiraj <adhirajsingh0105@gmail.com>
2026-07-01 22:59:02 +0530

    Update 03.cpp

--- DSA/02_Strings/03.cpp
+++ DSA/02_Strings/03.cpp
@@ -127,3 +127,30 @@ public:
         return ans;
     }
 };
+
+https://leetcode.com/problems/minimum-size-subarray-sum/description/
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+        int left = 0;
+        int sum = 0;
+        int minLen = INT_MAX;
+
+        for(int right = 0; right < nums.size(); right++) {
+
+            sum += nums[right];
+
+            while(sum >= target) {
+
+                minLen = min(minLen, right - left + 1);
+
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return (minLen == INT_MAX) ? 0 : minLen;
+    }
+};
