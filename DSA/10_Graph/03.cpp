9299b648a5927670137da4e99c79ba01bd4601e0
Adhiraj <adhirajsingh0105@gmail.com>
2026-07-20 22:48:20 +0530

    Update 03.cpp

--- DSA/10_Graph/03.cpp
+++ DSA/10_Graph/03.cpp
@@ -30,6 +30,33 @@ class Solution {
 };
 
 
+https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+
+class Solution {
+  public:
+  void dfsHelper(int node, vector<vector<int>>& adj, vector<bool> &visited, vector<int> &result){
+      visited[node] = true;
+      result.push_back(node);
+      
+      for(auto neighbour : adj[node]){
+          if(!visited[neighbour]){
+              dfsHelper(neighbour, adj, visited, result);
+          }
+      }
+  }
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int V = adj.size();
+        vector<bool> visited(V, false);
+        vector<int> result;
+        
+        dfsHelper(0, adj, visited, result);
+        return result;
+        
+        
+    }
+};
+
 https://leetcode.com/problems/number-of-provinces/
 
 class Solution {
